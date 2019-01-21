67d63416ac4f77751c1c1369ff8a4b54a09625c2
TernaryFish <sad.coder@yandex.ru>
2019-01-21 22:34:35 +0600

    123

--- game-blog/app/controllers/posts_controller.rb
+++ game-blog/app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: %i[index show]
 
   # GET /posts
   # GET /posts.json
@@ -69,6 +70,8 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :text)
+      params2 = params.require(:post).permit(:title, :text)
+      params2[user_id: current_user.id]
+      params2
     end
 end
--- game-blog/db/schema.rb
+++ game-blog/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_19_103422) do
+ActiveRecord::Schema.define(version: 2019_01_19_105023) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
