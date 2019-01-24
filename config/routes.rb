ece8c4d483eeb7357a552fb76b2786e10155a70b
TernaryFish <sad.coder@yandex.ru>
2019-01-24 22:25:03 +0600

    last

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :post_types
   devise_for :users
   resources :posts
+  get 'about', to: 'about#index'
   root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
